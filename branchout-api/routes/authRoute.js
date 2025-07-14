2335605e8e9dfebc1b2569dc9eaa0279df9df5a4
Taliyah Harvey <taliyah.harvey@taliyah-ltm6gk2.internal.salesforce.com>
2025-07-14 14:56:36 -0700

    created crud endpoints for user, verified w postman

--- branchout-api/routes/authRoute.js
+++ branchout-api/routes/authRoute.js
@@ -1,14 +1,11 @@
-const express = require('express');
+const express = require("express");
 const controller = require("../controllers/authController");
-const { authenticate, requireAdmin } = require('../middleware/authCheck'); 
+const { authenticate, requireAdmin } = require("../middleware/authCheck");
 const router = express.Router();
 
 router.post("/signup", controller.signup);
 router.post("/login", controller.login);
 router.post("/createAdmin", controller.createAdmin);
 router.post("/clerkSync", controller.clerkSync);
-
-
-
 
 module.exports = router;
--- branchout-api/routes/userRouter.js
+++ branchout-api/routes/userRouter.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const userController = require("../controllers/userController"); // Add this line
+const express = require("express");
+const userController = require("../controllers/userController");
 
-const { authenticate, requireAdmin } = require('../middleware/authCheck'); 
+const { authenticate, requireAdmin } = require("../middleware/authCheck");
 const router = express.Router();
 
 router.get("/profile", authenticate, userController.getUserProfile);
 router.get("/users", authenticate, requireAdmin, userController.getAllUsers);
+router.get("/:id", authenticate, requireAdmin, userController.getUser);
+router.put("/:id", authenticate, requireAdmin, userController.updateUser);
+router.delete("/:id", authenticate, requireAdmin, userController.deleteUser);
 
 module.exports = router;
