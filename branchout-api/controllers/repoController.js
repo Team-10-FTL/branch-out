c4bb84af56a0cf9d8a0f83ff2279c807d0dc8b09
Jaqueline Jurado <jaqueline.jurado@jaqueli-ltmdbv3.internal.salesforce.com>
2025-07-15 13:10:56 -0700

    initial commit of repo CRUD setup

---
+++ branchout-api/controllers/repoController.js
@@ -0,0 +1,17 @@
+const prisma = require("../models/prismaClient");
+
+
+// bare /repo routes should be admin only, repos that are user specific should be stored/posted/fetched from the user endpoints
+
+
+// get all repos (DONt ACTUALLY NEED THIS) bc its thousands of repos assuming we do the cron job
+
+
+
+
+// get a specific repo
+
+
+
+
+
