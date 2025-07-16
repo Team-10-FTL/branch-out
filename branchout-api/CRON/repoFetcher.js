4011c68fc1e6c4edc8f0a48ba109f236ea41659c
Jaqueline Jurado <jaqueline.jurado@jaqueli-ltmdbv3.internal.salesforce.com>
2025-07-16 15:36:40 -0700

    CRON job initial set up and modularized files

--- branchout-api/CRON/repoFetcher.js
+++ branchout-api/CRON/repoFetcher.js
@@ -1,16 +1,22 @@
+import axios from 'axios';
 
 
+// instead of having separate logic on the same edited file, use helpr functions in the helpers/ folder
+// here we can modularize the logic and PRs
 
-// GitHub API Logic
+// example from docs to call all information in a repo
+const getRepo = async () => {
+    const res = await githubApi.get('/repos/facebook/react');
+    console.log(res.data);
+};
+
+// example from docs on how to pull only certain repos from the api
+// https://api.github.com/search/repositories?q=stars:>1000&sort=stars
+
+// example endpoint from docs on how to pull open issues from a repo
+// https://api.github.com/repos/facebook/react/issues
 
 
-
-// Gemini API calls
 
 
 // Prisma DB inserts
-
-
-
-//A scheduler if you're running the 
-    // server persistently (or rely on external CRON triggers)
