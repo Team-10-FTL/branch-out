83a0e71e72e1977df696148005cbc0e3db4faa82
Jaqueline Jurado <jaqueline.jurado@jaqueli-ltmdbv3.internal.salesforce.com>
2025-07-16 16:14:00 -0700

    added logic to the github helper function to pull specific repos

---
+++ branchout-api/helpers/githubHelper.js
@@ -0,0 +1,22 @@
+const axios = require('axios'); // import axios for making HTTP requests to the GitHub API
+
+
+async function fetchReposWithFilters({ startDate, endDate }) {
+    // Filters: Must have a MIT license, must have at least one good first issue, and must be created between a startDate and endDate
+    const query = `license:mit good-first-issues:>0 created:${startDate}..${endDate}`;
+
+    // actual full search API URL - encoded component hides the full url
+    const url = `https://api.github.com/search/repositories?q=${encodeURIComponent(query)}&sort=stars&order=desc&per_page=10`;
+
+    const response = await axios.get(url, {
+        headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        Accept: "application/vnd.github+json"
+        }
+    });
+
+    return response.data.items; // returns a promise array of matching objects
+}
+
+module.exports = { fetchReposWithFilters }; // Export the function so it can be used in other parts of the project (cron job)
+
