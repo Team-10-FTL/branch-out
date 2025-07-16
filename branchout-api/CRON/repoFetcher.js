f775b8e871cb240aa73119b544fd7ff6abfa6784
Jaqueline Jurado <jaqueline.jurado@jaqueli-ltmdbv3.internal.salesforce.com>
2025-07-16 14:30:58 -0700

    set up cron job repo fetcher

---
+++ branchout-api/CRON/repoFetcher.js
@@ -0,0 +1,16 @@
+
+
+
+// GitHub API Logic
+
+
+
+// Gemini API calls
+
+
+// Prisma DB inserts
+
+
+
+//A scheduler if you're running the 
+    // server persistently (or rely on external CRON triggers)
