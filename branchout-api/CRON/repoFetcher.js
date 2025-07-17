d808e08b0361f6faa6657540563dc1d6ef4fc41d
Jaqueline Jurado <jaqueline.jurado@jaqueli-ltmdbv3.internal.salesforce.com>
2025-07-17 11:13:24 -0700

    parse through github data, set up gemini input

--- branchout-api/CRON/repoFetcher.js
+++ branchout-api/CRON/repoFetcher.js
@@ -1,22 +1,30 @@
+import {fetchReposWithFilters } from "../helpers/githubHelper";
+import { parseRepoForGemini } from "../helpers/repoParseHelper";
 import axios from 'axios';
-
 
 // instead of having separate logic on the same edited file, use helpr functions in the helpers/ folder
 // here we can modularize the logic and PRs
 
-// example from docs to call all information in a repo
-const getRepo = async () => {
-    const res = await githubApi.get('/repos/facebook/react');
-    console.log(res.data);
-};
+const repoFetcher = async () => {
+    // get last pulled information
+    const lastPulled = '2025-07-10'; // but pls replace this w an actual date
+    const currentDate = new DataTransfer().toISOString().slice(0, 10);
 
-// example from docs on how to pull only certain repos from the api
-// https://api.github.com/search/repositories?q=stars:>1000&sort=stars
+    // fetch repos from github (call the github helper)
+    const repos = await fetchReposWithFilters({ startDate: lastPulled, endDate: currentDate })
 
-// example endpoint from docs on how to pull open issues from a repo
-// https://api.github.com/repos/facebook/react/issues
+    for (const repo of repos){
+        // parse repo metadata
+        const parsedRepo = await parseRepoForGemini(repo); 
 
+        // get Gemini enriched data so
+        // do a call to the geminin helper function here with parsed Repo as the passed-in arguments
 
+        // there will be another function that cleans the data from the gemini output?? maybe, can try to write that in the same file
+        // then another function that pushes this data up to the database
 
+        console.log(parsedRepo); // for now to log if its parsing correctly
+    }
 
-// Prisma DB inserts
+}
+
